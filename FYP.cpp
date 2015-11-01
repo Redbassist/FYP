1989249671ef2a742653563ddc451359be0d4ca8
Matthew Gerig <mattgerig@gmail.com>
2015-11-01 18:12:58 +0000

    Add startup project with SFML and Box2d base in place

---
+++ FYP.cpp
@@ -0,0 +1,40 @@
+#include "stdafx.h"
+#include <iostream>
+#include <math.h>
+#include <Box2D/Box2D.h>
+#include "SFML/Graphics.hpp"
+#include "SFML/OpenGL.hpp"
+
+using namespace std;
+using namespace sf;
+
+int main()
+{
+	/** Prepare the window */
+	//int screenWidth = VideoMode::getDesktopMode().width;
+	//int screenHeight = VideoMode::getDesktopMode().height;	
+	int screenWidth = 1280;
+	int screenHeight = 720;
+	RenderWindow window(VideoMode(screenWidth, screenHeight, 32), "FYP");
+	window.setFramerateLimit(60);
+	View view = View(FloatRect(0, 0, screenWidth, screenHeight));
+	view.zoom(1);
+	window.setView(view);
+
+	b2Vec2 Gravity(0.f, 0.f);
+	b2World World(Gravity);
+
+	while (window.isOpen())
+	{
+		float32 timeStep = 1 / 20.0;      //the length of time passed to simulate (seconds)
+		int32 velocityIterations = 8;   //how strongly to correct velocity
+		int32 positionIterations = 3;   //how strongly to correct position
+
+		World.Step(timeStep, velocityIterations, positionIterations);
+		
+		window.clear();
+		window.display();
+
+	}
+}
+
